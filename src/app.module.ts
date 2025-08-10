f1245c25044ddf18d65b619b6358b815afcaba80
andmatcha <59753159+andmatcha@users.noreply.github.com>
2025-08-10 19:10:33 +0900

    環境変数とnecordの初期設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NecordModule } from 'necord';
+import { IntentsBitField } from 'discord.js';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development.local'],
+      cache: true,
+    }),
+    NecordModule.forRoot({
+      token: process.env.DISCORD_TOKEN ?? '',
+      intents: [IntentsBitField.Flags.Guilds],
+      development: [process.env.DISCORD_DEVELOPMENT_GUILD_ID ?? ''],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
